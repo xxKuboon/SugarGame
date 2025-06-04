b45d4808d7c046aaa717ff7ed93b1157982b67a8
Looser <73042304+xxKuboon@users.noreply.github.com>
2025-06-04 19:52:09 +0200

    dodane składniki oraz historyjki

--- main.cpp
+++ main.cpp
@@ -15,48 +15,69 @@ public:
     string dobra_reakcja;
     string srednia_reakcja;
     string zla_reakcja;
+    string dramatyczna_reakcja;
 
     void losuj() {
         string imiona[] = { "Anna", "Jan", "Ola", "Tomek", "Zosia" };
         string nazwiska[] = { "Nowak", "Kowalski", "Wisniewski", "Kaczmarek", "Dabrowska" };
         string historyjki[] = {
-            "Wyszedlem tylko po chleb, a wrocilem z wata cukrowa, trzema krasnalami i nowym tatuazem z napisem 'Slodycz to bunt'. Mama nie uwierzyla, ale babcia tak.",
-            "Zakochalam sie w cukierniku. Robil landrynki, ale byl niemowa i porozumiewal sie przez dzwiek dzwoneczkow. Porwali go hipsterzy i zmusili do robienia krowek z tofu.",
-            "Moj sasiad siedzi w szafie i krzyczy, ze jest batonem. Policja juz byla, ale dali mu mleko i poszli. Tez czasem czuje sie jak Prince Polo.",
-            "Babcia mowi, ze w PRL-u cukierki smakowaly jak wspomnienie niedzieli i smog. Dzis dzieci jedza zelki i nie wiedza, co to ostatni Michalek.",
-            "Kiedys zjadlam worek cukierkow i uslyszalam glosy. To byla reklama z radia sasiada, ale przysiegam: mowila do mnie landrynka. Miala glos Dody.",
-            "Moj chlopak pracuje w fabryce lizakow. Kazdy smak to emocja. Jagodowy to wstyd, truskawkowy to trauma, a coli nie robia, bo to bylby manifest."
+            "Hej, słyszałem o Tobie od mojego kolegi! Podobno masz najlepsze cukierki w mieście! To prawda, że pierwszy za darmo?",
+            "Wiem, że wiszę Ci trochę kasy za ostatnie cukierki, ale co powiesz na wymianę. Mogę oddać Ci moją mamę…",
+            "Mój chłopak to prawdziwy romantyk. Poznaliśmy się na spotkaniu anonimowych miłośników cukierków! Siedzieliśmy w kółku i opowiadaliśmy o naszych ulubionych smakach! To nasza druga randka. Możesz coś dla nas przygotować?",
+            "No siema. Potrzebuję małej pomocy. Mój przyjaciel nie je cukierków już od kilku tygodni. Wiem, że strasznie je lubi, ale chyba wstydzi się tego przyznać. Na pewno będzie mu miło po małym prezencie!",
+            "Potrzebuję wsparcia. Jutro mam zawody sportowe i myślę, że mały cukierek mógłby okazać się nieocenioną pomocą! Nie, nie nazywam się Karaś!",
+            "Przyśnił mi się sen. Beata Kozidrak przybiła mi piątkę, a potem podwiozła mnie na przystanek tramwajowy. Po drodze mieliśmy wypadek – patrzę, a jestem w szpitalu… Co? Oj, faktycznie mam wenflon w łapie…",
+            "Zamknąłem brata w lodówce, bo zabrał mi ostatniego cukierka. Swoją drogą – nie potrzebujesz może nerki?",
+            "Moja babcia biega po mieszkaniu i krzyczy, że jest beretem. Nie do końca rozumiem co to znaczy. Zawsze wydawało mi się, że jest moherem. ",
+            "Nie uwierzysz w tę historię! Wychodzę wczoraj po chleb, a wracam z taczką pełną Rumunów. I co ja teraz z nimi zrobię? Przecież nie hoduję na balkonie żadnych róż, a poza tym mam za daleko do Wrocławia!",
+            "Ej, słyszałeś ten huk? A, w takim razie musiało mi się tylko przesłyszeć…",
+            "Ręce trzęsą mi się od dziecka, a teraz i tak jest dużo lepiej! Kiedyś złamałem nos nauczycielowi. Nie, te historie nie są powiązane…",
+            "Próbowałeś kiedyś przykręcić koło bananem? To znacznie łatwiejsze niż się wydaje. Potrzeba tylko dużo cierpliwości i dwa słoiki masła orzechowego!",
+            "To koniec! Nie przyjęli mnie na ASP. To chyba znak od losu, że pora zająć się polityką…",
+            "Zawsze mam przy sobie łyżeczkę, bo piję dużo kawy i brzydzę się cudzych sztućców. A zapalniczka leżała na drodze.",
+            "Oglądałeś Matrixa? Którą tabletkę byś wybrał? Ja obie."
         };
 
         string dobra_reakcje[] = {
     "Smakuje jak wakacje w Radomiu z 2003!",
     "O kurde, jakby mi ktos wlal szczescie do ust.",
-    "To lepsze niz darmowe pr�bki w Biedrze!",
-    "Jakbym jadla wat� cukrow� posypan� d�wi�kiem techno.",
+    "To lepsze niz darmowe próbki w Biedrze!",
+    "Jakbym jadla watę cukrową posypaną dźwiękiem techno.",
     "Mniam jak pierwsze piwo na klatce schodowej!",
     "Jeszcze! I jeszcze! I moze byc z plastikiem nawet!",
-    "Cukierek jak sen na dopalaczach � ale legalny.",
-    "Czuj�, �e wraca mi wiara w ludzko��. I w s�odycze."
+    "Cukierek jak sen na dopalaczach – ale legalny.",
+    "Czuję, że wraca mi wiara w ludzkość. I w słodycze.",
+    "9",
+    "10"
 };
         string srednia_reakcje[] = {
-    "No nie wiem, jakby mi�towa pasta do z�b�w udawala lizaka.",
+    "No nie wiem, jakby miętowa pasta do zębów udawala lizaka.",
     "Jest okej... ale bez dreszczy.",
-    "Smakuje jak herbata u cioci: ani z�a, ani dobra, po prostu jest.",
-    "To chyba z tych cukierk�w, co si� je z grzeczno�ci.",
-    "Jakby kto� rozpu�ci� marzenie w chlorowanej wodzie.",
+    "Smakuje jak herbata u cioci: ani zła, ani dobra, po prostu jest.",
+    "To chyba z tych cukierków, co się je z grzeczności.",
+    "Jakby ktoś rozpuścił marzenie w chlorowanej wodzie.",
     "Taki smak 'meh', ale przynajmniej nie kopie jak gaz z kaloryfera.",
-    "Jakbym jadla wat�, co le�a�a za szaf�, ale z cukrem."
+    "Jakbym jadla watę, co leżała za szafą, ale z cukrem.",
+     "9",
+    "10"
 };
         string zla_reakcje[] = {
     "Fuj! Smakuje jak sen o zepsutym jogurcie!",
     "To jest przestepstwo przeciwko kubkom smakowym.",
     "Czy to... guma do zucia po babci?",
-    "Mam wrazenie, �e zjadlam zapach klatki schodowej.",
+    "Mam wrazenie, że zjadlam zapach klatki schodowej.",
     "To nie jest cukierek, to trauma w folii.",
-    "Smak jak z autobusu linii 145 w lipcu � bez klimy.",
-    "Wypluj mnie, blagam � krzyczy moj jezyk.",
-    "Czuje sie jakbym zdradzi�a swoj� diete i swoja godnosc."
+    "Smak jak z autobusu linii 145 w lipcu – bez klimy.",
+    "Wypluj mnie, blagam – krzyczy moj jezyk.",
+    "Czuje sie jakbym zdradziła swoją diete i swoja godnosc.",
+     "9",
+    "10"
 };
+        string dramatyczna_reakcje[] = {
+        "ajajajaja",
+         "9",
+        "10"
+        };
 
         //losowanie imienia, nazwiska i historyjki
         imie = imiona[rand() % 5];
@@ -66,6 +87,7 @@ public:
         dobra_reakcja = dobra_reakcje[rand() % 5];
         srednia_reakcja = srednia_reakcje[rand() % 5];
         zla_reakcja = zla_reakcje[rand() % 5];
+        dramatyczna_reakcja = dramatyczna_reakcje[rand() % 5];
     }
 
     void zareaguj(bool poprawne) {
@@ -88,26 +110,16 @@ public:
 
      static void wyswietlSklep() {
 
-        string listaSkladnikow[] = { "woda z kaloryfera",
-    "cukier z PRL-u",
-    "aromat biedy",
-    "mleko w proszku 3.0",
-    "cola turbo light",
-    "syrop z melancholii",
-    "smog malinowy",
-    "gluten syntetyczny",
-    "kofeina uliczna",
-    "mieta z kebaba",
-    "popiol z papierosa mentolowego",
-    "olejek disco polo",
-    "parowka waniliowa",
-    "brokat z bazaru",
-    "sok z dramatyzmu",
-    "margaryna wspomnien",
-    "piana z energetyka",
-    "aromat porazki" };
+        string listaSkladnikow[] = {
+    "cukier puder",
+    "susz owocowy",
+    "tęczowy barwnik",
+    "miód waniliowy",
+    "switamina C",
+    "cynamon"
+     };
         cout << "=== SKLADNIKI DO WYBORU ===" << endl;
-        for (int i = 0; i < 8; ++i) {
+        for (int i = 0; i < 6; ++i) {
             cout << "- " << listaSkladnikow[i] << " (malo / duzo)" << endl;
         }
         cout << "=======================" << endl;
@@ -150,9 +162,9 @@ public:
 
         klient.pokaz();
         cout << "Zamowienie do wykonania: ";
-        receptura.pokaz(); //f do wystwietlania receptury ktora sie wylosowala - odkomentuj zeby zobaczyc czy program dzia�a dobrze
+        receptura.pokaz(); //f do wystwietlania receptury ktora sie wylosowala - odkomentuj zeby zobaczyc czy program działa dobrze
         cout << "--------------------------------\n";
-        Skladnik::wyswietlSklep(); // to jest funkcja statyczna wiec mozna j� wywo�a� poprzez "::" klasa::nazwa_funkcji
+        Skladnik::wyswietlSklep(); // to jest funkcja statyczna wiec mozna ją wywołać poprzez "::" klasa::nazwa_funkcji
         cout << "--------------------------------\n";
         cout << "\nDodaj 3 skladniki (nazwa + malo/duzo):\n";
         for (int i = 0; i < 3; ++i) {
@@ -163,7 +175,7 @@ public:
             getline(cin, wybor[i].ilosc);
         }
 
-        bool trafione = porownaj(); // tu trzeba jakos zrobic ten scoring? p�ki co sprawdza tylko czy receptura git czy nie. Mo�e w klasie jakas funkcje z scoringiem trzeba?
+        bool trafione = porownaj(); // tu trzeba jakos zrobic ten scoring? póki co sprawdza tylko czy receptura git czy nie. Może w klasie jakas funkcje z scoringiem trzeba?
         klient.zareaguj(trafione);
     }
 
